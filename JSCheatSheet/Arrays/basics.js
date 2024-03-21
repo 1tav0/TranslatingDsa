6324292c69ec74f3aa7d78ec6c3540e03ed8b0d7
Tavo Cruz <madecruz2@gmail.com>
2024-03-21 15:03:12 -0700

    manipulating objects in the dom using arrays

--- JSCheatSheet/Arrays/basics.js
+++ JSCheatSheet/Arrays/basics.js
@@ -1,14 +1,20 @@
 let list = [
-  'milk',
-  'eggs',
-  'butter',
-  'bread',
-  'bacon'
+  { text: 'milk', cost: 4, need: false },
+  { text: 'eggs', cost: 10, need: true },
+  { text: 'butter', cost: 5, need: false },
+  { text: 'bread', cost: 3, need: true },
+  { text: 'bacon', cost: 12, need: false },
 ];
 
 let html = '';
+let total = 0;
+
 for (let i = 0; i < list.length; i++){
-  html += `<li>${list[i]}</li>`
+  if (list[i].need) {
+    html += `<li>${list[i].text}</li>`
+    total += list[i].cost
+  }
 }
 
 document.getElementById('list').innerHTML = html;
+document.getElementById('cost').innerHTML = `$${total}`;
